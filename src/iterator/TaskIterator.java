498f0ab6c11061526c76c04656dda497c8172892
Tommaso Brigaglia <91462387+toretto2002@users.noreply.github.com>
2025-05-28 00:29:02 +0200

    Implementa TaskIterator con supporto per filtri e gestione delle attività

--- src/iterator/TaskIterator.java
+++ src/iterator/TaskIterator.java
@@ -1,5 +1,40 @@
 package iterator;
 
-public class TaskIterator {
+import java.util.Iterator;
+import java.util.List;
+import java.util.function.Predicate;
+
+import model.Task;
+
+public class TaskIterator implements Iterator<Task> {
+
+    private final List<Task> tasks;
+    private final Predicate<Task> filter;
+    private int currentIndex = 0;
+
+    public TaskIterator(List<Task> tasks, Predicate<Task> filter) {
+        this.tasks = tasks;
+        this.filter = filter;
+        moveToNextValid();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return currentIndex < tasks.size();
+    }
+
+    @Override
+    public Task next() {
+        Task current = tasks.get(currentIndex);
+        currentIndex++;
+        moveToNextValid();
+        return current;
+    }
+
+    private void moveToNextValid() {
+        while (currentIndex < tasks.size() && !filter.test(tasks.get(currentIndex))) {
+            currentIndex++;
+        }
+    }
 
 }
